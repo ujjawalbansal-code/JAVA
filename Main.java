bd2c4616e6e403011cea4f46e16789120922479b
Ujjawal Bansal <ujjawalbansal4816@gmail.com>
2026-06-03 01:52:58 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+//        System.out.print("hello world");
+//        System.out.println(1234);
+
+//        Scanner input =new Scanner(System.in);
+//        IO.println(input.nextLine());
+//        System.out.println(input.next());
+
+//        primitive datatypes are data types that you can-not break further into another datatypes
+//        int rollNo = 64;        4 bytes           identifier(rollNo)=literals(64)
+//        char letter = 'r';
+//        float marks = 98.67F;    4 bytes
+//        double largeDecimalNumber = 4567654.4567;       8 bytes
+//        long largeInteger = 34567834567876543L;         8 bytes
+//        boolean check = true;
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter some input: ");
+//        int rollNo = input.nextInt();
+//        System.out.println("Your roll number is :" + rollNo);
+//        int a =234_000_000;  234 billion
+//        System.out.println(a);
+//        String name = input.nextLine();
+//        System.out.println(name);
+//        float marks = input.nextFloat();     564.6758463 -> 564.67584   ||   10 -> 10.0
+//        System.out.println(marks);
+
+//        Scanner input = new Scanner(System.in);
+//        float num1 = input.nextFloat();
+//        float num2 = input.nextFloat();
+//        float sum = num1 + num2;                       here is type conversion from int to float
+//        System.out.println("sum of number "+num1+" & "+num2+" = "+ sum);
+
+//        type conversion         automatically convert it int to float but not float to int
+//        type should be compatable
+//        destination type should be greater-then the source type
+
+//        type casting(nanoing conversions)
+//        compressing the bigger number into smaller type explicitly
+//        int num =(int)(67.56f);
+//        System.out.println(num);
+
+//        automatic type promotion in expressions
+//        int a = 259;
+//        byte b = (byte)(a);      max value is 256 so it take mode of a value by this casting 259%256->3
+//        System.out.println(b);
+
+//        byte a = 40;
+//        byte b = 50;
+//        byte c = 100;
+//        int d = a * b / c;      in expressions byte automatically convert into integer
+//        System.out.println(d);
+
+//        byte b =50;
+//        b=b*2;   this will give an error bca b*2 is now an integer but you want to store in byte that's why it gives error
+
+//        int number = 'a';            if you give character then int will give its unicode value
+//        System.out.println(number);
+
+//        rule for type promotion
+//        all the byte short and character value are converted into integer
+//        if any one of the operand is long the whole operation will be promoted to the long
+//        if it is float then whole operation will be promoting to the float
+//        if it is double then entire evaluation is evaluated to be double
+//        System.out.println(3*5.6f); -> float
+
+//        int a = 10;
+//        if (a ==10) {
+//            System.out.println("hello world");
+//        }
+
+//        int count = 1;
+//        while (count != 5){
+//            System.out.println(count);
+//            count++;
+//        }
+
+//        for(int count = 1; count !=5;count++){
+//            System.out.println(count);
+//        }
+
+    }
+}
