e6c87a69f59ae0bdaf9d72fb35e988e5b9b43e40
Ujjawal Bansal <ujjawalbansal4816@gmail.com>
2026-06-04 01:21:51 +0530

    Add files via upload
    This is an conditional statement and loops

---
+++ Demo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+//        multiple if-else
+//        int salary = 25400;
+//        if (salary>10000){
+//            salary+=2000;
+//        } else if (salary>20000) {
+//            salary+=3000;
+//        }else {
+//            salary+=1000;
+//        }
+//        System.out.println(salary);
+
+//        for (int num=1;num<=5;num+=1){
+//            System.out.println(num);
+//        }
+
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        for (int num = 1; num <= n; num++) {
+//            System.out.println(num);
+
+//        int i=1;
+//        while(i<=5){
+//            System.out.println(i);
+//            i+=1;
+//        }
+
+        int n=1;
+        do{
+            System.out.println(n);
+            n++;
+        } while (n<=5);
+    }
+}
